f068e85e9b08271b03f96a484328c4b17a9c2bd4
ikhwan69 <ikhwanrahman76@gmail.com>
2022-12-31 16:56:54 +0700

    fixing chlidren typescript

--- layout/layout.tsx
+++ layout/layout.tsx
@@ -1,5 +1,5 @@
 
-export default function Layout({ children }) {
+export default function Layout({ children }: any) {
     return (
         <div className="bg-hero-login bg-no-repeat bg-cover bg-center w-80%">
             {children}
